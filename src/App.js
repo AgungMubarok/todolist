9ac78e6262fafcf49047268df289bb66f1b36dc7
AgungMubarok <agungmubarok555@gmail.com>
2020-07-23 17:15:47 +0700

    fisrt commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import ToDo from './ToDo';
+import './styles.css';
+
+function App() {
+  return (
+    <div className ="isi">
+      <h1>To Do List</h1>
+      <ul><ToDo/></ul>
+    </div>
+  );
+}
+
+export default App;
